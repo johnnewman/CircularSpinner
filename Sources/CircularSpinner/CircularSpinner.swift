7c477be17e3d322fce8bb79572a214f307510528
John Newman <newman719@gmail.com>
2024-03-26 22:20:24 -0400

    Adding additional previews. Improving the rotation logic.

--- Sources/CircularSpinner/CircularSpinner.swift
+++ Sources/CircularSpinner/CircularSpinner.swift
@@ -8,36 +8,38 @@
 import SwiftUI
 import Combine
 
-struct CircularSpinner<Background: ShapeStyle, Foreground: ShapeStyle>: View {
+public struct CircularSpinner<Background: ShapeStyle, Foreground: ShapeStyle>: View {
     
     let lineWidth: CGFloat
-    
-    let animationDuration: TimeInterval
-    
+        
     let backgroundStyle: Background
     
     let foregroundStyle: Foreground
     
+    let spinAnimation: Animation
+    
+    /// This timer will fire each time the spin animation finishes.
     let timer: Publishers.Autoconnect<Timer.TimerPublisher>
     
-    @State private var animate: Bool = false
-        
+    /// This accumulates the ongoing angle to keep the spinner moving.
     @State private var previousAngle: Angle = .zero
     
+    /// This is toggled between values to oscillate the foreground circle's length.
     @State private var trimLength: Length = .short
-        
-    @State private var animation: SpinAnimation
     
-    init(lineWidth: CGFloat = 20,
-         animationDuration: TimeInterval = 1.75,
-         backgroundStyle: Background,
-         foregroundStyle: Foreground) {
+    public init(lineWidth: CGFloat = 10,
+                animationDuration: TimeInterval = 1.75,
+                backgroundStyle: Background = Color.secondary,
+                foregroundStyle: Foreground = Color.primary) {
         self.lineWidth = lineWidth
-        self.animationDuration = animationDuration
         self.backgroundStyle = backgroundStyle
         self.foregroundStyle = foregroundStyle
+        self.spinAnimation = .interpolatingSpring(duration: animationDuration)
         self.timer = Timer.publish(every: animationDuration, on: .main, in: .default).autoconnect()
-        self.animation = .short(duration: animationDuration)
+    }
+    
+    var currentAngle: Angle {
+        previousAngle + .degrees(720) + .degrees(-(360 * trimLength.value) / 2)
     }
     
     func circle<T: ShapeStyle>(style: T, length: CGFloat) -> some View {
@@ -50,43 +52,70 @@ struct CircularSpinner<Background: ShapeStyle, Foreground: ShapeStyle>: View {
                 )
             )
             .foregroundStyle(style)
-    }
-    
-    var angle: Angle {
-        previousAngle + .degrees(720) + .degrees(-90 - (360 * trimLength.value) / 2)
+            .rotationEffect(.degrees(-90))
     }
     
     func cycleAnimation() {
         previousAngle += .degrees(720)
         trimLength.toggle()
     }
-        
-    var body: some View {
+    
+    public var body: some View {
         ZStack {
             // Background circle.
             circle(style: backgroundStyle, length: 1)
             
-            // Animated forground stroke.
+            // Animated forground.
             circle(style: foregroundStyle, length: trimLength.value)
-                .rotationEffect(angle)
-                .animation (
-                    animation.value,
-                    value: previousAngle
-                )
+                .rotationEffect(currentAngle)
+                .animation(spinAnimation, value: previousAngle)
                 .onAppear(perform: cycleAnimation)
                 .onReceive(timer) { _ in
                     cycleAnimation()
                 }
         }
+        .padding(lineWidth / 2)
     }
 }
 
 #Preview {
-    CircularSpinner(
-        lineWidth: 10,
-        backgroundStyle: Color.gray,
-        foregroundStyle: Color.black
-    )
-    .frame(width: 50, height: 50)
-    .padding()
+    VStack {
+        CircularSpinner(
+            lineWidth: 10,
+            animationDuration: 2,
+            backgroundStyle: .black,
+            foregroundStyle: .thickMaterial.shadow(.inner(radius: 3))
+        )
+        
+        CircularSpinner(
+            lineWidth: 10,
+            animationDuration: 1.75,
+            backgroundStyle: Color.secondary,
+            foregroundStyle: Color.primary
+        )
+        
+        CircularSpinner(
+            lineWidth: 10,
+            animationDuration: 1.5,
+            backgroundStyle: Color.black,
+            foregroundStyle: LinearGradient(
+                colors: [.purple, .red],
+                startPoint: .leading,
+                endPoint: .trailing
+            )
+        )
+        
+        CircularSpinner(
+            lineWidth: 10,
+            animationDuration: 1.25,
+            backgroundStyle: .radialGradient(
+                colors: [.green, .red],
+                center: .center,
+                startRadius: 25-10,
+                endRadius: 25
+            ),
+            foregroundStyle: .cyan.shadow(.drop(radius: 2))
+        )
+    }
+    .frame(width: 50)
 }
