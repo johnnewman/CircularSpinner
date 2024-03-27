c66c2d95382cd3ecbb1d38bca07c671beabf7555
John Newman <newman719@gmail.com>
2024-03-27 18:01:31 -0400

    Lowering the swift tools version.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "CircularSpinner",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v13),
         .iOS(.v16)
     ],
     products: [
